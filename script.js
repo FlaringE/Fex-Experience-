975b5d9f9ff8a95309b193f5e2a8d11efe8aa16b
FEX <133824609+FlaringE@users.noreply.github.com>
2025-11-16 12:10:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+alert("You are about enter about to enter Fex Resource site")
+alert("One stop place for coding resources for learner's")
+alert("Please this is test site no share untill resources are confirmed")
